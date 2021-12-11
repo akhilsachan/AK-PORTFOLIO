a29fe8470e9883ee35ffc91ec0037367bfe477a1
akhilsachan <akhildrm123@gmail.com>
2021-12-11 09:50:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// typing animation
+
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Front-end Developer", "Back-end Developer", "Web Designer", "Freelancer", "SEO Expert", "Full Stack Developer", "Graphic Designer", "Wordpress Developer", "Web Developer", "Blogger", "YouTube SEO Expert","Digital Marketer", "Logo Designer"],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
+});
+
+$(document).ready(function(){
+    var typed = new Typed(".typing2", {
+        strings: ["Front-end Developer", "Back-end Developer", "Web Designer", "Freelancer", "SEO Expert", "Full Stack Developer", "Graphic Designer", "Wordpress Developer", "Web Developer", "Blogger", "YouTube SEO Expert", "Digital Marketer", "Logo Designer"],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
