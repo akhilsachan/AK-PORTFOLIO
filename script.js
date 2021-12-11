1899067e0418a3714a6fc0ef2e81dcd9046c5f74
akhilsachan <akhildrm123@gmail.com>
2021-12-11 10:06:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 $(document).ready(function(){
     var typed = new Typed(".typing", {
-        strings: ["Front-end Developer", "Back-end Developer", "Web Designer", "Freelancer", "SEO Expert", "Full Stack Developer", "Graphic Designer", "Wordpress Developer", "Web Developer", "Blogger", "YouTube SEO Expert","Digital Marketer", "Logo Designer"],
-        typeSpeed: 50,
+        strings: ["Software Engineer", "Front-end Developer", "Back-end Developer", "Web Designer", "Freelancer", "Full Stack Developer", "Wordpress Developer", "Web Developer",  "SEO Expert", "Blogger", "Graphic Designer", "YouTube SEO Expert","Digital Marketer", "Logo Designer"],
+        typeSpeed: 30,
         backSpeed: 30,
         loop: true
     });
@@ -11,8 +11,8 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     var typed = new Typed(".typing2", {
-        strings: ["Front-end Developer", "Back-end Developer", "Web Designer", "Freelancer", "SEO Expert", "Full Stack Developer", "Graphic Designer", "Wordpress Developer", "Web Developer", "Blogger", "YouTube SEO Expert", "Digital Marketer", "Logo Designer"],
-        typeSpeed: 50,
+         strings: ["Software Engineer", "Front-end Developer", "Back-end Developer", "Web Designer", "Freelancer", "Full Stack Developer", "Wordpress Developer", "Web Developer",  "SEO Expert", "Blogger", "Graphic Designer", "YouTube SEO Expert","Digital Marketer", "Logo Designer"],
+        typeSpeed: 30,
         backSpeed: 30,
         loop: true
     });
